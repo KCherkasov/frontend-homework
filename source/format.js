ec8c0e7c8742d02d3a0e7b580c709e7fd1231655
Kirill Cherkasov <kvcherk@live.ru>
2017-08-30 02:31:22 +0300

    applied changes listed in review (except sort issue)

--- source/format.js
+++ source/format.js
@@ -2,34 +2,31 @@
 
 // функция преобразования исходных данных в строку заданной длины
 // (с заполнением пробелами слева до требуемой длины)
-const prepend_spaces = function(source, needed_length) {
+const prepend_spaces = (source, needed_length) => {
   let result = String(source);
-  while (result.length < needed_length) {
-    result = ' ' + result;
-  }
-  return result;
+  let length_diff = needed_length - result.length;
+  return (length_diff <= 0) ? result : ' '.repeat(length_diff).concat(result);
 }
 
 // функция форматированного вывода массива чисел в cols колонок с выравниванием по правому краю
-const format = function(input, cols) {
+const format = (input, cols) => {
   // инициализируем и заполняем массив макс. ширин колонок
-  let cols_char_width = [];
-  for (let i = 0; i < cols; ++i) {
-    cols_char_width.push(0);
-  }
+  let cols_char_width = [].fill.call({length : cols}, 0);
 
   // ищем максимально длинную строку в каждом столбце
   for (let i = 0; i < input.length; ++i) {
     let item_length = String(input[i]).length;
-    if (cols_char_width[i % cols] < item_length) {
-      cols_char_width[i % cols] = item_length;
+    let col_index = i % cols;
+    if (cols_char_width[col_index] < item_length) {
+      cols_char_width[col_index] = item_length;
     }
   }
 
   // формируем выводимую строку
   let output = new String('');
   for (let i = 0; i < input.length; ++i) {
-    output += prepend_spaces(input[i], cols_char_width[i % cols]) + ((i + 1 == input.length) ? '' : (i % cols + 1 == cols) ? '\n' : ' ');
+    let col_index = i % cols;
+    output += prepend_spaces(input[i], cols_char_width[col_index]) + ((i + 1 === input.length) ? '' : (col_index + 1 === cols) ? '\n' : ' ');
   }
   return output;
 }

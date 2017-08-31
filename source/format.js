41da5c55da0fc0d75f2ce88a74155ad2623608f2
Kirill Cherkasov <kvcherk@live.ru>
2017-08-31 09:39:33 +0300

    fixed codestyle; applied template strings and reduce()

--- source/format.js
+++ source/format.js
@@ -1,32 +1,30 @@
-'use strict';
+ 'use strict';
 
 // функция преобразования исходных данных в строку заданной длины
 // (с заполнением пробелами слева до требуемой длины)
-const prepend_spaces = (source, needed_length) => {
+const prependSpaces = (source, neededLength) => {
   let result = String(source);
-  let length_diff = needed_length - result.length;
-  return (length_diff <= 0) ? result : ' '.repeat(length_diff).concat(result);
+  let lengthDiff = neededLength - result.length;
+  return (lengthDiff <= 0) ? result : `${' '.repeat(lengthDiff)}${result}`;
 }
 
 // функция форматированного вывода массива чисел в cols колонок с выравниванием по правому краю
 const format = (input, cols) => {
   // инициализируем и заполняем массив макс. ширин колонок
-  let cols_char_width = [].fill.call({length : cols}, 0);
+  let colsCharWidth = [].fill.call({length : cols}, 0);
 
   // ищем максимально длинную строку в каждом столбце
   for (let i = 0; i < input.length; ++i) {
-    let item_length = String(input[i]).length;
-    let col_index = i % cols;
-    if (cols_char_width[col_index] < item_length) {
-      cols_char_width[col_index] = item_length;
+    let itemLength = String(input[i]).length;
+    let colIndex = i % cols;
+    if (colsCharWidth[colIndex] < itemLength) {
+      colsCharWidth[colIndex] = itemLength;
     }
   }
 
   // формируем выводимую строку
-  let output = new String('');
-  for (let i = 0; i < input.length; ++i) {
-    let col_index = i % cols;
-    output += prepend_spaces(input[i], cols_char_width[col_index]) + ((i + 1 === input.length) ? '' : (col_index + 1 === cols) ? '\n' : ' ');
-  }
-  return output;
+  return input.reduce((previousValue, currentValue, index, array) => {
+           let colIndex = index % cols;
+           return `${previousValue}${prependSpaces(currentValue, colsCharWidth[colIndex])}${(index + 1 === array.length) ? '' : (colIndex + 1 === cols) ? '\n' : ' '}`;
+         }, '');
 }
